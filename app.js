2a43cfaaa52fe70ba35083b8c7ac4f80a95e657a
sadbatya <sadbatya@mail.ru>
2023-08-04 10:05:30 +0200

    done task

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
-const openModalBtn = document.querySelector('#open__modal--window')
-const modal = document.querySelector('.modal')
-const closeModalBtn = document.querySelector('.modal__close--icon')
-const sendInfoBtn = document.querySelector('.send__btn')
-const body = document.querySelector('body')
+const openModalBtn = document.querySelector('#open__modal--window');
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.modal__close--icon');
+const sendInfoBtn = document.querySelector('.send__btn');
+const body = document.querySelector('body');
+const prevBtn = document.querySelector('.btn__prev');
+const nextBtn = document.querySelector('.btn__next');
+const sliderLine = document.querySelector('.section3__line');
+console.log(sliderLine);
 
-openModalBtn.addEventListener('click', () => {
-  modal.classList.add('open')
-  body.style.position = 'fixed'
-})
+function modalWindow() {
+  openModalBtn.addEventListener('click', () => {
+    modal.classList.add('open');
+    body.style.position = 'fixed';
+  });
 
-closeModalBtn.addEventListener('click', () => {
-  modal.classList.remove('open')
-  body.style.position = ''
-})
+  closeModalBtn.addEventListener('click', () => {
+    modal.classList.remove('open');
+    body.style.position = '';
+  });
+
+  sendInfoBtn.addEventListener('click', () => {
+    modal.classList.remove('open');
+    body.style.position = '';
+  });
+}
+
+modalWindow();
+
+let offset = 0;
+
+nextBtn.addEventListener('click', () => {
+  offset += 785;
+  if (offset > 3140) {
+    offset = 0;
+  }
+  sliderLine.style.left = -offset + 'px';
+});
+
+prevBtn.addEventListener('click', () => {
+  offset -= 785;
+  if (offset < 0) {
+    offset = 3140;
+  }
+  sliderLine.style.left = -offset + 'px';
+});
 
 
-sendInfoBtn.addEventListener('click', () => {
-  modal.classList.remove('open')
-  body.style.position = ''
-})
-
-alert('Здравствуйте!Меня зовут Владимир! Буду очень рад обратному фидбеку!')
-setTimeout(() => {
-  alert('Вот мои контакты для связи 8-909-776-47-38')
-  alert('Telegram @sadbatyaa')
-}, 7000)
